3b849051113486dd28421ebdd99bb0d162eb1f80
PAWB <vanny.com>
2023-03-17 10:23:37 +0700

    v1.0

--- loop.py
+++
@@ -1,8 +0,0 @@
-def cekGanjilGenap(int):
-    if int % 2==1:
-        print(int, "adalah ganjil")
-    else:
-        print(int, "adalah genap")
-
-for i in range(1, 100+1):
-    cekGanjilGenap(i)       
